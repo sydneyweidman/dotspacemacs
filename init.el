3f1808e02f8a3bf68d901b8d3a5bee87e7c444d4
Sydney Weidman <sydney.weidman@gmail.com>
2016-03-31 07:10:02 -0500

    Added zoom back in to the user-config section

--- init.el
+++ init.el
@@ -240,7 +240,6 @@ It is called immediately after `dotspacemacs/init'.  You are free to put almost
 any user code here.  The exception is org related code, which should be placed
 in `dotspacemacs/user-config'."
 
-
 )
 
 (defun dotspacemacs/user-config ()
@@ -267,8 +266,27 @@ layers configuration. You are free to put any user code."
   ;; Org agenda files
   (setq org-agenda-files '("~/org/work.org" "~/org/personal.org"))
 
+  ;; Zoom
+  (setq zoom 0)
+  (setq maxzoom 4)
+  (while (< zoom maxzoom)
+    (zoom-frm-in)
+    (setq zoom (1+ zoom)))
 
  )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
